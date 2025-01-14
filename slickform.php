07a8ee2ef03c7c3a39b3b4df11ee68cd7fc22c11
T5Technologies <Tfivetechnologies@gmail.com>
2025-01-15 01:17:03 +0530

    website changes

--- slickform.php
+++ slickform.php
@@ -3,7 +3,7 @@
 
 if(isset($_REQUEST["isvalid"])){
 	
-	$youremail = "app.marko@gmail.com"; 
+	$youremail = "app.marko@gmail.com"; // Enter your email here!!
 	$usersname = $_POST["usersname"];
 	$usersemail = $_POST["usersemail"];
 	$usersphonenumber = $_POST["usersphone"];
